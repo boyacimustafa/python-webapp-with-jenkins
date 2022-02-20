d8e4e89cbdebdebe1580eff40cd5d64d8c9e56b0
Mustafa Boyar <boyar_mustafa@yahoo.com>
2022-02-19 22:13:12 -0700

    update webapp

--- webapp.py
+++ webapp.py
@@ -1,16 +1,10 @@
 from flask import Flask, render_template
-import random
 
 app = Flask(__name__)
 
-# list of cat images
-images = [
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1200px-Python-logo-notext.svg.png",
-]
-
 @app.route('/')
 def index():
-    url = random.choice(images)
+    url = "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1200px-Python-logo-notext.svg.png"
     return render_template('index.html', url=url)
 
 if __name__ == "__main__":
